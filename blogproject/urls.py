76c1af7d0f36cf0ebdd15566a11bf3a17f23273c
Hoteol <dbsjh0637@gmail.com>
2020-06-30 14:28:08 +0900

    urls modify

--- blogproject/urls.py
+++ blogproject/urls.py
@@ -14,12 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 import blogapp.views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', blogapp.views.home, name='home'),
-    path('new/', blogapp.views.new, name='new'),
-    path('detail/<int:blog_id>/', blogapp.views.detail, name='detail'),
+    path('blogapp/', include('blogapp.urls')),
 ]
